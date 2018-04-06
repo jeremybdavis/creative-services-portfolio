804d7d7f1a8a19246544dc1a92de4b71036c8916
Jeremy Davis <jdavis129@gmail.com>
2018-04-06 13:29:58 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## HCA Creative Services Portfolio Site
+Built using Wordpress API and Gatsby JS
---
+++ gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  siteMetadata: {
+      title: 'HCA Creative Services',
+      subtitle: `Fetch Data From Local WP Install`,
+  },
+  plugins: [
+      'gatsby-plugin-react-helmet',
+      {
+          resolve: "gatsby-source-wordpress",
+          options: {
+              baseUrl: "hcacs.wpengine.com",
+              protocol: "http",
+              hostingWPCOM: false,
+              useACF: true,
+              verboseOutput: true
+          }
+      }
+  ],
+};
