b9c815f440c4479c0f098e53134f4bca73050c7a
Jeremy Davis <jdavis129@gmail.com>
2018-04-09 17:01:36 -0500

    added ACF fields to home hero, more homepage and navigation work

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
 import Link from 'gatsby-link'
+import './index.css'
 
 class IndexPage extends Component {
     render() {
         const data = this.props.data
+        const acfFields = data.wordpressAcfPages.acf
 
         return(
             <div>
                 <div className="hero">
-                    <h1>We Are Creative Services</h1>
+                    <img src={acfFields.home_hero_bg_img.source_url} alt="Hero Image"/>
+                    <h1>{acfFields.home_hero_headline}</h1>
                 </div>
                 <div className="section">
                     <h1>Who We Are</h1>
@@ -19,3 +22,16 @@ class IndexPage extends Component {
 }
 
 export default IndexPage
+
+export const homePageQuery = graphql`
+  query homePageQuery{
+    wordpressAcfPages {
+      acf {
+        home_hero_headline
+        home_hero_bg_img{
+            source_url
+        }
+      }
+    }
+  }
+`
