60c929d07369c2e68c4e6fc6c1d21ef2257d42c9
Jeremy Davis <jdavis129@gmail.com>
2018-04-20 14:35:24 -0500

    updated gatsby version

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -6,16 +6,18 @@ import './templates.css'
 class PostsTemplate extends Component {
     render() {
         const data = this.props.data;
+        const acfFields = data.wordpressAcfPages.acf
 
         return(
-            <div>
+            <div className="blog">
                 <div className="nav-bg"></div>
-                <h1>Posts</h1>
+                <h1>Blog</h1>
+                <h3>{acfFields.blog_subheadline}</h3>
 
                 {data.allWordpressPost.edges.map(({node}) => (
                     <div key={node.slug} className={"post"} style={{ marginBottom: 50 }}>
                         <Link to={node.slug}>
-                            <h3>{node.title}</h3>
+                            <h3 dangerouslySetInnerHTML={{__html: node.title}}></h3>
                         </Link>
 
                         <div className={"post-content"} dangerouslySetInnerHTML={{__html: node.excerpt}} />
@@ -47,6 +49,12 @@ export const pageQuery = graphql`
                     slug
                     date(formatString: "MMMM DD, YYYY")
                 }
+            }
+        }
+
+        wordpressAcfPages {
+            acf {
+                blog_subheadline
             }
         }
     }
