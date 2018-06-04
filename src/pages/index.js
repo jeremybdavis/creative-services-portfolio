5e6b26f61f393f812dcadf6e9a4ecfd4cfdcbac1
Jeremy Davis <jdavis129@gmail.com>
2018-06-04 16:48:56 -0500

    style tweaks to navigation and homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,17 +13,21 @@ class IndexPage extends Component {
             <div>
                 <div className="hero">
                     <img src={acfFields.home_hero_bg_img.source_url} alt="Hero Image"/>
-                    <h1>{acfFields.home_hero_headline}</h1>
+                    <div className="hero-content">
+                        <h1>{acfFields.home_hero_headline}</h1>
+                        <h2>{acfFields.home_hero_tagline}</h2>
+                    </div>
                 </div>
 
                 <div className="section who-we-are">
                     <div className="content">
                         <h2>{acfFields.home_about_headline}</h2>
                         <div dangerouslySetInnerHTML={{__html: acfFields.home_about_content}}/>
+                        <Link to="/what-we-do/" className="btn">View More</Link>
                     </div>
                 </div>
 
-                <div className="section" style={{backgroundColor: '#ede9e6'}}>
+                <div className="section case-studies">
                     <h2>{acfFields.home_portfolio_headline}</h2>
                     <div className="latest-work">
                         {caseStudies.edges.slice(-3).reverse().map(({node}) => (
@@ -35,14 +39,13 @@ class IndexPage extends Component {
                                             <span>View Project</span>
                                         </div>
                                     </div>
-                                    <h3 dangerouslySetInnerHTML={{__html: node.title}}></h3>
                                 </Link>
                             </div>
                         ))}
                     </div>
                 </div>
 
-                <div className="section">
+                <div className="section" style={{backgroundColor: '#ede9e6'}}>
                     <h2>Blog</h2>
                     <div className="home-blog">
                         {posts.edges.slice(0,3).map(({node}) => (
@@ -75,6 +78,7 @@ export const homePageQuery = graphql`
     wordpressAcfPages {
       acf {
         home_hero_headline
+        home_hero_tagline
         home_hero_bg_img{
             source_url
         }
